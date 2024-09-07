cedbdcb2ba227d7dd1593b3349d19c6a2564b09b
Asadbek A'lamov <104184218+alamovAsadbek@users.noreply.github.com>
2024-09-07 23:05:25 +0500

    change

--- pages/role/admin/admin_restaurant_menu.py
+++ pages/role/admin/admin_restaurant_menu.py
@@ -5,6 +5,7 @@ from main_files.decorator.decorator_func import log_decorator
 class AdminRestaurantMenu:
     @log_decorator
     def show_all_restaurants(self):
+        print("Waiting...")
         query = '''
         SELECT * FROM restaurants
         INNER JOIN users u on restaurants.OWNER_ID = u.ID
