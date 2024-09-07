3a7f8cf3a575d645694f6c32055ada38213ab4b4
Asadbek A'lamov <104184218+alamovAsadbek@users.noreply.github.com>
2024-09-07 18:34:43 +0500

    change get_data func

--- pages/role/admin/admin_user_menu.py
+++ pages/role/admin/admin_user_menu.py
@@ -5,12 +5,12 @@ from main_files.decorator.decorator_func import log_decorator
 class AdminUserMenu:
     @log_decorator
     def get_data(self):
-        pass
+        query = '''
+                SELECT * FROM users
+                '''
+        all_users = execute_query(query, fetch='all')
+        return all_users
 
     @log_decorator
     def show_all_users(self):
-        query = '''
-        SELECT * FROM users
-        '''
-        all_users = execute_query(query, fetch='all')
-        print(all_users)
+        pass
