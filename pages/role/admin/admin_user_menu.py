c68d125737101d4fcd451089c54989fb978eb4e3
Asadbek A'lamov <104184218+alamovAsadbek@users.noreply.github.com>
2024-09-07 18:39:21 +0500

    change

--- pages/role/admin/admin_user_menu.py
+++ pages/role/admin/admin_user_menu.py
@@ -1,10 +1,15 @@
-from components.pagination.pagination import Pagination
+from main_files.database.db_setting import execute_query
 from main_files.decorator.decorator_func import log_decorator
 
 
 class AdminUserMenu:
-    def __init__(self):
-        self.__user_pagination = Pagination()
+    @log_decorator
+    def get_data(self):
+        query = '''
+                SELECT * FROM users
+                '''
+        all_users = execute_query(query, fetch='all')
+        return all_users
 
     @log_decorator
     def show_all_users(self):
