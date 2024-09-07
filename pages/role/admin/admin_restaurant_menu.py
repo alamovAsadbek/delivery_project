4773aa21a3eeaea562504601101b0e26f86b4c47
Asadbek A'lamov <104184218+alamovAsadbek@users.noreply.github.com>
2024-09-07 23:53:38 +0500

    change

--- pages/role/admin/admin_restaurant_menu.py
+++ pages/role/admin/admin_restaurant_menu.py
@@ -31,14 +31,14 @@ class AdminRestaurantMenu:
         phone_number = int(input("Enter phone number ( +998 ): ").strip())
         company_fee = int(input("Enter company fee (0-100): ").strip())
         print("Waiting...")
-        query = '''
-        SELECT * FROM users
-        WHERE role='owner'
-        '''
         while True:
+            query = '''
+                    SELECT * FROM users
+                    WHERE role='owner'
+                    '''
             result = execute_query(query, fetch='all')
             pagination = Pagination(table_name='users', data=result,
-                                    table_keys=['id, first_name', 'last_name', 'username', 'phone_number'],
+                                    table_keys=['id', 'first_name', 'last_name', 'username', 'phone_number'],
                                     display_keys=['ID', 'FIRST NAME', 'LAST NAME', 'USERNAME', 'PHONE NUMBER'])
             pagination.page_tab()
             print('\n1. Add new owner\n2. Enter user id')
