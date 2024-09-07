a49d9dd6874e8a4932b8b3a8e21c60a0805401d9
Asadbek A'lamov <104184218+alamovAsadbek@users.noreply.github.com>
2024-09-07 18:34:13 +0500

    create get_data func

--- pages/role/admin/admin_user_menu.py
+++ pages/role/admin/admin_user_menu.py
@@ -4,6 +4,10 @@ from main_files.decorator.decorator_func import log_decorator
 
 class AdminUserMenu:
     @log_decorator
+    def get_data(self):
+        pass
+
+    @log_decorator
     def show_all_users(self):
         query = '''
         SELECT * FROM users
