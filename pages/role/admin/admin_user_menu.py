0531bd4417aa3e9ea20e4fd8a0e08f4602923825
Asadbek A'lamov <104184218+alamovAsadbek@users.noreply.github.com>
2024-09-07 22:45:22 +0500

    change show_all_users func

--- pages/role/admin/admin_user_menu.py
+++ pages/role/admin/admin_user_menu.py
@@ -13,8 +13,10 @@ class AdminUserMenu:
     @log_decorator
     def show_all_users(self):
         pagination = Pagination(table_name='users',
-                                table_keys=['id', 'first_name', 'last_name', 'phone_number', 'role', 'created_at'],
-                                display_keys=['ID', 'FIRST_NAME', 'LAST_NAME', 'PHONE_NUMBER', 'ROLE', 'created_at'])
+                                table_keys=['id', 'first_name', 'last_name', 'username', 'phone_number', 'role',
+                                            'created_at'],
+                                display_keys=['ID', 'FIRST_NAME', 'LAST_NAME', 'USERNAME', 'PHONE_NUMBER', 'ROLE',
+                                              'created_at'])
         pagination.page_tab()
         return True
 
