d405a490d4abd41a07a8e0db61b9223c8e0ef18b
Asadbek A'lamov <104184218+alamovAsadbek@users.noreply.github.com>
2024-09-07 12:33:49 +0500

    change login func

--- pages/auth/auth.py
+++ pages/auth/auth.py
@@ -1,7 +1,11 @@
+import hashlib
+
 from main_files.decorator.decorator_func import log_decorator
 
 
 class Auth:
     @log_decorator
     def login(self):
-        pass
+        username: str = input('Username: ').strip()
+        password: str = hashlib.sha256(input('Password: ').strip().encode('utf-8')).hexdigest()
+
