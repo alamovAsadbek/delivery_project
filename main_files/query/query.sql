0b6b5a658818576dec86cee9d3e958b198345440
Asadbek A'lamov <104184218+alamovAsadbek@users.noreply.github.com>
2024-09-07 12:57:08 +0500

    change sql file

---
+++ main_files/query/query.sql
@@ -0,0 +1,32 @@
+-- users tableni yasab olish uchun query
+CREATE TABLE IF NOT EXISTS users
+(
+    ID
+    BIGSERIAL
+    PRIMARY
+    KEY,
+    FIRST_NAME
+    VARCHAR
+(
+    255
+) NOT NULL,
+    LAST_NAME VARCHAR
+(
+    255
+) NOT NULL,
+    username VARCHAR
+(
+    255
+) NOT NULL,
+    password VARCHAR
+(
+    255
+) NOT NULL,
+    phone_number BIGINT NOT NULL,
+    ROLE VARCHAR
+(
+    255
+) NOT NULL,
+    IS_LOGIN BOOLEAN NOT NULL DEFAULT FALSE,
+    CREATED_AT TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+    );
