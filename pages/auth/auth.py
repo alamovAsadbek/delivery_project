c7fb57d2fed730148ef53665b9a12b44517f04a7
Asadbek A'lamov <104184218+alamovAsadbek@users.noreply.github.com>
2024-09-07 12:36:34 +0500

    change __init__ func

--- pages/auth/auth.py
+++ pages/auth/auth.py
@@ -5,9 +5,11 @@ from main_files.decorator.decorator_func import log_decorator
 
 class Auth:
     def __init__(self):
-        self.__admin = {'username': 'admin', 'password': 'admin'}
+        self.__admin = {'username': 'admin', 'password': hashlib.sha256('admin'.encode()).hexdigest()}
 
     @log_decorator
     def login(self):
         username: str = input('Username: ').strip()
         password: str = hashlib.sha256(input('Password: ').strip().encode('utf-8')).hexdigest()
+        if username == self.__admin['username'] and password == self.__admin['password']:
+            pass
