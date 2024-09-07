4970f28a48330ebf904b40e6753ec8187bf4fafa
Asadbek A'lamov <104184218+alamovAsadbek@users.noreply.github.com>
2024-09-07 13:10:43 +0500

    change admin_menu

--- main.py
+++ main.py
@@ -38,7 +38,14 @@ def admin_menu():
     '''
     print(text)
     try:
-        pass
+        user_input = int(input('Choose menu: ').strip())
+        if user_input == 1:
+            pass
+        elif user_input == 2:
+            pass
+        elif user_input == 3:
+            print('Exit')
+            auth_menu()
     except Exception as e:
         print(f'Error: {e}')
         admin_menu()
