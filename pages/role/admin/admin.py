43123814275397608539206f159705945b2c83cf
Asadbek A'lamov <104184218+alamovAsadbek@users.noreply.github.com>
2024-09-07 22:53:58 +0500

    change

--- pages/role/admin/admin.py
+++ pages/role/admin/admin.py
@@ -1,10 +1,12 @@
 from main_files.decorator.decorator_func import log_decorator
+from pages.role.admin.admin_restaurant_menu import AdminRestaurantMenu
 from pages.role.admin.admin_user_menu import AdminUserMenu
 
 
 class Admin:
     def __init__(self):
         self.__user_menu = AdminUserMenu()
+        self.__restaurant_menu = AdminRestaurantMenu()
 
     # for user menu
 
