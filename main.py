32e3aac1bae8bb7f207283f453ac6262c6ee9059
Asadbek A'lamov <104184218+alamovAsadbek@users.noreply.github.com>
2024-09-07 12:48:19 +0500

    change auth_menu func

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 from main_files.decorator.decorator_func import log_decorator
+from pages.auth.auth import Auth
 
 
 @log_decorator
 def auth_menu():
+    print('Waiting...')
+    auth = Auth()
     text = '''
 1. Register 
 2. Login    
@@ -14,7 +17,11 @@ def auth_menu():
         if user_input == 1:
             pass
         elif user_input == 2:
-            pass
+            result_login = auth.login()
+            if not result_login['is_login']:
+                auth_menu()
+            elif result_login['role'] == 'admin':
+                pass
         elif user_input == 3:
             pass
     except Exception as e:
