70beb6933b31da5dfb0e0eac61dd7d835d35cb9c
Asadbek A'lamov <104184218+alamovAsadbek@users.noreply.github.com>
2024-09-07 12:34:22 +0500

    create __init__ func in auth.py

--- pages/auth/auth.py
+++ pages/auth/auth.py
@@ -4,8 +4,10 @@ from main_files.decorator.decorator_func import log_decorator
 
 
 class Auth:
+    def __init__(self):
+        pass
+
     @log_decorator
     def login(self):
         username: str = input('Username: ').strip()
         password: str = hashlib.sha256(input('Password: ').strip().encode('utf-8')).hexdigest()
-
