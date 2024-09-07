f1df94db95013b14b3ef6a31b30b2cca9d86ca84
Asadbek A'lamov <104184218+alamovAsadbek@users.noreply.github.com>
2024-09-07 18:29:58 +0500

    create show_all_users func in admin.py

--- pages/role/admin/admin.py
+++ pages/role/admin/admin.py
@@ -3,5 +3,9 @@ from main_files.decorator.decorator_func import log_decorator
 
 class Admin:
     @log_decorator
+    def show_all_users(self):
+        pass
+
+    @log_decorator
     def create_restaurant(self):
         pass
