f3d5f0dcbe7d55c07deb3ce5db9d066cadf74091
Asadbek A'lamov <104184218+alamovAsadbek@users.noreply.github.com>
2024-09-07 14:59:43 +0500

    change sql file

--- main_files/query/query.sql
+++ main_files/query/query.sql
@@ -19,3 +19,17 @@ CREATE TABLE IF NOT EXISTS users
 -- Dastur birinchi marta ishlagan payt hamma userlarni is_loginni false qilish uchun yani logoout
 UPDATE users
 SET IS_LOGIN= FALSE;
+
+-- Restaurants tableni yaratish uchun query
+CREATE TABLE IF NOT EXISTS restaurants
+(
+    ID           BIGSERIAL PRIMARY KEY,
+    NAME         VARCHAR(255) NOT NULL,
+    USERNAME     VARCHAR(255) NOT NULL UNIQUE,
+    PASSWORD     VARCHAR(255) NOT NULL,
+    ROLE         VARCHAR(255) NOT NULL,
+    PHONE_NUMBER BIGINT       NOT NULL UNIQUE,
+    COMPANY_FEE  BIGINT       NOT NULL,
+    OWNER_ID     VARCHAR(255) NOT NULL REFERENCES users (ID) ON DELETE CASCADE,
+    CREATED_AT   TIMESTAMP    NULL DEFAULT CURRENT_TIMESTAMP
+)
