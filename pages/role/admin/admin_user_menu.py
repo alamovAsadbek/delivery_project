51ed71cf69f57626d91b230d06a38ee41e80e249
Asadbek A'lamov <104184218+alamovAsadbek@users.noreply.github.com>
2024-09-07 18:33:10 +0500

    change show_all_users func

--- pages/role/admin/admin_user_menu.py
+++ pages/role/admin/admin_user_menu.py
@@ -1,7 +1,12 @@
+from main_files.database.db_setting import execute_query
 from main_files.decorator.decorator_func import log_decorator
 
 
 class AdminUserMenu:
     @log_decorator
     def show_all_users(self):
-        pass
+        query = '''
+        SELECT * FROM users
+        '''
+        all_users = execute_query(query, fetch='all')
+        print(all_users)
