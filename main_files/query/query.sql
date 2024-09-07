14ea0f9141cf5c54b07ea4beee0cf307b40502bc
Asadbek A'lamov <104184218+alamovAsadbek@users.noreply.github.com>
2024-09-07 14:57:23 +0500

    change sql file

--- main_files/query/query.sql
+++ main_files/query/query.sql
@@ -2,34 +2,19 @@
 CREATE TABLE IF NOT EXISTS users
 (
     ID
-    BIGSERIAL
-    PRIMARY
-    KEY,
+                 BIGSERIAL
+        PRIMARY
+            KEY,
     FIRST_NAME
-    VARCHAR
-(
-    255
-) NOT NULL,
-    LAST_NAME VARCHAR
-(
-    255
-) NOT NULL,
-    username VARCHAR
-(
-    255
-) NOT NULL,
-    password VARCHAR
-(
-    255
-) NOT NULL,
-    phone_number BIGINT NOT NULL UNIQUE,
-    ROLE VARCHAR
-(
-    255
-) NOT NULL,
-    IS_LOGIN BOOLEAN NOT NULL DEFAULT FALSE,
-    CREATED_AT TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
-    );
+                 VARCHAR(255)        NOT NULL,
+    LAST_NAME    VARCHAR(255)        NOT NULL,
+    username     VARCHAR(255) UNIQUE NOT NULL,
+    password     VARCHAR(255)        NOT NULL,
+    phone_number BIGINT UNIQUE       NOT NULL,
+    ROLE         VARCHAR(255)        NOT NULL,
+    IS_LOGIN     BOOLEAN             NOT NULL DEFAULT FALSE,
+    CREATED_AT   TIMESTAMP           NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
 
 -- Dastur birinchi marta ishlagan payt hamma userlarni is_loginni false qilish uchun yani logoout
 UPDATE users
--- pages/auth/tables/tables.py
+++ pages/auth/tables/tables.py
@@ -10,7 +10,7 @@ class Tables:
             ID BIGSERIAL PRIMARY KEY,
             FIRST_NAME VARCHAR(255) NOT NULL,
             LAST_NAME VARCHAR(255) NOT NULL,
-            username VARCHAR(255) NOT NULL,
+            username VARCHAR(255) NOT NULL UNIQUE,
             password VARCHAR(255) NOT NULL,
             phone_number BIGINT NOT NULL UNIQUE,
             ROLE VARCHAR(255) NOT NULL,
@@ -23,4 +23,11 @@ class Tables:
 
     @log_decorator
     def create_restaurants_table(self):
-        pass
+        query='''
+        CREATE TABLE IF NOT EXISTS restaurants (
+        ID BIGSERIAL PRIMARY KEY,
+        NAME VARCHAR(255) NOT NULL,
+        USERNAME VARCHAR(255) NOT NULL UNIQUE,
+        
+        )
+        '''
