4407cf704b78a964bd213b68a3a7c6d66699a2f4
Asadbek A'lamov <104184218+alamovAsadbek@users.noreply.github.com>
2024-09-07 18:42:46 +0500

    create __init__ func in admin.py

--- pages/role/admin/admin.py
+++ pages/role/admin/admin.py
@@ -2,6 +2,9 @@ from main_files.decorator.decorator_func import log_decorator
 
 
 class Admin:
+    def __init__(self):
+        pass
+
     @log_decorator
     def show_all_users(self):
         pass
