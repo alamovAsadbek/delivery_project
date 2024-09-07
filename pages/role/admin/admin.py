da207f74f25a0564b90ee358c461b3459a7017b2
Asadbek A'lamov <104184218+alamovAsadbek@users.noreply.github.com>
2024-09-07 18:43:34 +0500

    change __init__ func

--- pages/role/admin/admin.py
+++ pages/role/admin/admin.py
@@ -1,9 +1,10 @@
 from main_files.decorator.decorator_func import log_decorator
+from pages.role.admin.admin_user_menu import AdminUserMenu
 
 
 class Admin:
     def __init__(self):
-        pass
+        self.__user_menu = AdminUserMenu()
 
     @log_decorator
     def show_all_users(self):
