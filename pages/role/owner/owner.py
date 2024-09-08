f07d93199edab887baeb49693d18a6bd4ccc2dde
Asadbek A'lamov <104184218+alamovAsadbek@users.noreply.github.com>
2024-09-08 19:20:29 +0500

    create __init__ file

--- pages/role/owner/owner.py
+++ pages/role/owner/owner.py
@@ -2,6 +2,9 @@ from main_files.decorator.decorator_func import log_decorator
 
 
 class OwnerRestaurant:
+    def __init__(self):
+        pass
+
     @log_decorator
     def show_all_branches(self):
         pass
