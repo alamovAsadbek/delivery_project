5f1c52ad063e5278d1dfd36fe6766dc1fb4cbdac
Asadbek A'lamov <104184218+alamovAsadbek@users.noreply.github.com>
2024-09-08 19:07:33 +0500

    change

--- main.py
+++ main.py
@@ -219,7 +219,20 @@ def owner_branch_menu():
     '''
     print(text)
     try:
-        pass
+        user_input = int(input('Choose menu: '))
+        if user_input == 1:
+            pass
+        elif user_input == 2:
+            pass
+        elif user_input == 3:
+            pass
+        elif user_input == 4:
+            pass
+        elif user_input == 5:
+            return owner_restaurant_menu()
+        else:
+            print("Wrong input")
+        owner_branch_menu()
     except Exception as e:
         print(f'Error: {e}')
         owner_branch_menu()
