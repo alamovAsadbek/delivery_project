713e1d4bb1f6f31a83cbacab7b2cd2f196323c3b
Asadbek A'lamov <104184218+alamovAsadbek@users.noreply.github.com>
2024-09-08 19:21:02 +0500

    change

--- pages/role/owner/owner.py
+++ pages/role/owner/owner.py
@@ -1,9 +1,10 @@
 from main_files.decorator.decorator_func import log_decorator
+from pages.role.owner.branch import Branch
 
 
 class OwnerRestaurant:
     def __init__(self):
-        pass
+        self.__branch = Branch()
 
     @log_decorator
     def show_all_branches(self):
