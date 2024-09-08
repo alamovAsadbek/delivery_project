66814bee7f557efd8bafb194dd55fc16a774ee70
Asadbek A'lamov <104184218+alamovAsadbek@users.noreply.github.com>
2024-09-08 22:25:32 +0500

    change create_branch

--- pages/role/owner/branch.py
+++ pages/role/owner/branch.py
@@ -47,4 +47,12 @@ class Branch:
 
     @log_decorator
     def create_branch(self):
-        pass
+        get_restaurant = self.select_restaurant()
+        if get_restaurant is None:
+            return False
+        print(f"\nRestaurant: {get_restaurant['name']}\n")
+        branch_name = input("Enter branch name to create: ").strip()
+        location = input("Enter location to create: ").strip()
+        phone_number = input("Enter phone number (+998): ").strip()
+        print("Creating branch...")
+
